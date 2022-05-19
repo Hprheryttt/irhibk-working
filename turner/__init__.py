123bee7042a99a4aca73697567b9122f7dcd6f46
dr-qaatil <76719386+dr-qaatil@users.noreply.github.com>
2022-05-19 18:05:25 +0530

    Update __init__.py

--- turner/__init__.py
+++ turner/__init__.py
@@ -140,6 +140,7 @@ try:
     AUTO_DELETE_MESSAGE_DURATION = int(getConfig('AUTO_DELETE_MESSAGE_DURATION'))
     TELEGRAM_API = getConfig('TELEGRAM_API')
     TELEGRAM_HASH = getConfig('TELEGRAM_HASH')
+    AUTHORIZED_CHATS = [int(i) for i in os.environ.get("AUTH_CHATS", "").split(" ")]
 except:
     LOGGER.error("One or more env variables missing! Exiting now")
     exit(1)
